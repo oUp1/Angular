a44282d1c0c6e3c6d9fd64acc2f56ab789da4067
Oleksandr Sukach <OSukach@on.seo>
2020-07-02 18:43:33 +0300

    Added grid boxes

--- application/src/app/modules/home/home.component.ts
+++ application/src/app/modules/home/home.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { timer } from 'rxjs';
+import { isRegExp } from 'util';
 
 
 @Component({
@@ -7,7 +9,32 @@ import { Component } from '@angular/core';
     styleUrls: ['./home.component.scss']
 })
 
-export class HomeComponent  {
+export class HomeComponent implements OnInit {
 
+    count: number = 60;
+    isBiggest: boolean =  false;
+
+    getWraperStyle() {
+        let backColor: string = `radial-gradient(circle, rgba(2,0,36,1) 0%, rgba(0,0,0,0.5718662464985995) ${this.count}%, rgba(181,255,0,1) 100%)`
+        let styles = {
+            "background": backColor
+        };
+        return styles;
+    }
+
+    observableTimer() {
+        const source = timer(100, 20);
+        const abc = source.subscribe(val => {
+
+            this.count = this.isBiggest ? --this.count : ++this.count;
+
+            if (this.count >= 100) this.isBiggest = true;
+            if (this.count <= 0) this.isBiggest = false;
+        });
+    }
+
+    ngOnInit(): void {
+        this.observableTimer();
+    }
 
 }
--- application/src/app/modules/home/home.module.ts
+++ application/src/app/modules/home/home.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { HomeComponent } from './home.component';
+import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
     declarations: [HomeComponent],
     imports: [
-        RouterModule
+        BrowserModule,
+        RouterModule,
+        FormsModule
     ]
 })
 export class HomeModule { }
